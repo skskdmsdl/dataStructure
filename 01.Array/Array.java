c5582d9d2c872005a7d4cf9d50faab791db95021
skskdmsdl <skskdmsdl@gmail.com>
2023-01-02 21:53:47 +0900

    array example

---
+++ 01.Array/Array.java
@@ -0,0 +1,10 @@
+// foreach loops
+public class Array {
+    public static void main(String[] args) {
+        double[] arrList = {2.0, 3.3, 5.5, 7.1};
+        // Print all the array elements
+        for (double element: myList) {
+            System.out.println(element);
+        }
+    }
+}
